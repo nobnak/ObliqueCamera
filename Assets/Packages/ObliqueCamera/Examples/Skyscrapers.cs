02411a8fc8838054c9d5d4b9460f863833eb0181
Nobuyuki Nakata <nobu.nakata@gmail.com>
2016-06-22 14:31:03 +0900

    no message

---
+++ Assets/Packages/ObliqueCamera/Examples/Skyscrapers.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ObliqueCameraSystem {
+    public class Skyscrapers : MonoBehaviour {
+        public GameObject buildingfab;
+        public float radius = 5f;
+        public float size = 2f;
+        public float sizeVariation = 0.2f;
+        public int count = 100;
+
+        GameObject[] _buildings;
+
+    	// Use this for initialization
+    	void Start () {
+            _buildings = new GameObject[count];
+            for (var i = 0; i < count; i++) {
+                var b = _buildings[i] = Instantiate (buildingfab);
+                var tr = b.transform;
+                var sampleInCircle = Random.insideUnitCircle;
+                tr.SetParent (transform, false);
+                tr.localPosition = radius * new Vector3 (sampleInCircle.x, 0f, sampleInCircle.y);
+                tr.localRotation = Quaternion.Euler (0f, Random.Range (0f, 360f), 0f);
+                tr.localScale = (1f + sizeVariation * Random.value) * size * Vector3.one;
+            }
+    	}
+    }
+}
---
+++ Assets/Packages/ObliqueCamera/Examples/TestObliqueCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ObliqueCameraSystem {
+    [RequireComponent(typeof(Collider))]
+    public class TestObliqueCamera : MonoBehaviour {
+        public float scale = 0.1f;
+        public Camera targetCam;
+
+        Collider _collider;
+
+        void Start() {
+            _collider = GetComponent<Collider> ();
+        }
+    	void Update () {
+            if (Input.GetMouseButton (0)) {
+                var posScreen = Input.mousePosition;
+                var posCam = targetCam.transform.position;
+                posCam.x = scale * (posScreen.x - 0.5f * Screen.width);
+                posCam.z = scale * (posScreen.y - 0.5f * Screen.height);
+                targetCam.transform.position = posCam;
+            }
+    	}
+    }
+}
